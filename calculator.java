e4a0b0192cb3e65b568933a8ae1b991d5e597004
HemanthAndru <hemanthandru6666@gmail.com>
2021-08-21 21:20:44 +0530

    basic calci second commit

--- calculator.java
+++ calculator.java
@@ -4,6 +4,6 @@ class calculator{
    int a=22;
    int b=34;
    System.out.println("Multiplication of two numbers :"+ a*b);
-   System.out.println("Devision of two numbers :" b/a);
+   System.out.println("Devision of two numbers :"+ b/a);
 }
 }
