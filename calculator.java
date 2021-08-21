282e18bacb145b53dd259378ac5c5951566cc061
HemanthAndru <hemanthandru6666@gmail.com>
2021-08-21 21:15:12 +0530

    basic calci functions

---
+++ calculator.java
@@ -0,0 +1,9 @@
+class calculator{
+   public static void main(String args[]){
+
+   int a=22;
+   int b=34;
+   System.out.println("Multiplication of two numbers :"+ a*b);
+   System.out.println("Devision of two numbers :" b/a);
+}
+}
